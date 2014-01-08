f0aa15e516616aa442fed1c2399bcb5251e64aae
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-08 11:44:43 +0100

    Adds main_server/MainServer.py

--- main_server/Device.py
+++ main_server/Device.py
@@ -3,7 +3,11 @@
 
 class Device:
 
-    def __init__(self, id):
+    def __init__(self, main_server, id):
+        self.main_server = main_server
         self.id = id
         self.ignored = true
+
+    def save(self):
+        self.main_server.devices[self.id] = id
 
