05a5017259c7709adcbd5fed5f07526641b45585
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-12 00:30:04 +0100

    mv main_server/test_enocean.py -> enocean/test_devices.py

--- app/enocean/test_devices.py
+++ app/enocean/test_devices.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import enocean
+import devices
 
 
 def test_thermometer():
     data_bytes = [0x00, 0x84, 0x99, 0x0F]
-    reading = enocean.Thermometer.reading_from_data_bytes(None, data_bytes)
+    reading = devices.Thermometer.reading_from_data_bytes(None, data_bytes)
 
     assert reading.humidity == 52.8
     assert reading.temperature == 24.48
