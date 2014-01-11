c2e9141c6000e0f72e41c7f33f24044f571d0f08
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-11 18:39:38 +0100

    Adds mongodb managment in app/model/

---
+++ app/model/components.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import mongoengine
+
+
+class Device(mongoengine.Document):
+    device_id = mongoengine.StringField(required=True, unique=True)
+    ignored = mongoengine.BooleanField(default=True)
+
+    meta = {'allow_inheritance': True}
+
+
+class Reading(mongoengine.Document):
+    device = mongoengine.ReferenceField(Device, required=True)
+
+    meta = {'allow_inheritance': True}
