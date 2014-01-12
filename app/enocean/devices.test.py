985077e0b982ecd61e7c507702644d6c0731ad9e
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-01-12 23:00:48 +0100

    Merges enocean/test_devices.py in enocean/devices.test.py

--- app/enocean/devices.test.py
+++ app/enocean/devices.test.py
@@ -10,9 +10,9 @@ import model
 import devices
 
 
-if __name__ == '__main__':
-    db = mongoengine.connect('test')
-    db.drop_database('ghome_model_tests')
+def test_mongoengine():
+    db = mongoengine.connect('ghome_enocean_test')
+    db.drop_database('ghome_enocean_test')
 
     thermometer = devices.Thermometer(device_id="Hello")
 
@@ -29,3 +29,13 @@ if __name__ == '__main__':
 
     assert thermometerValue in model.devices.Thermometer.Reading.objects(device=thermometer)
 
+def test_thermometer():
+    data_bytes = [0x00, 0x84, 0x99, 0x0F]
+    reading = devices.Thermometer.reading_from_data_bytes(None, data_bytes)
+
+    assert reading.humidity == 52.8
+    assert reading.temperature == 24.48
+
+if __name__ == "__main__":
+    test_mongoengine()
+    test_thermometer()
