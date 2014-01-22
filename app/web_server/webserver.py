99c07a17794271cac80ff208c3a22a4cd87b2090
Adrien Duffy <adrien.duffy-coissard@insa-lyon.fr>
2014-01-22 09:53:11 +0100

    added api8tracks search in main_server instead of webserver, but still need to fix generator end problem

--- app/web_server/webserver.py
+++ app/web_server/webserver.py
@@ -4,9 +4,6 @@
 import json
 import sys
 sys.path.insert(0, '..')
-sys.path.insert(0, '../../libs/py8tracks')
-
-from py8tracks import API8tracks
 from twisted.web.xmlrpc import Proxy,reactor
 from twisted.web import xmlrpc, server
 
@@ -137,17 +134,11 @@ def lamps():
 def playMusic():
     if request.method == 'POST':
         form=request.form
-        tag =  [form.get(val) for val in ['tag']]
-        print tag
-
-        api=API8tracks(config.api_8tracks)
-
-        mixset = api.mixset(tags=tag, sort='popular')
-        url=search_music_generator(mixset)
-
+        tags =  [form.get(val) for val in ['tag']]
+        print tags
         #url= "http://streamTaMaman.wav" # [stream.data['track_file_stream_url'] for stream in mixset.mixes[0].tracks_cache]
         server= xmlrpclib.Server("http://"+str(config.main_server.ip)+':'+str(config.main_server.rpc_port))
-        server.raspi.find_music_url(0,next(url))#callRemote('raspi.find_music_url',0,url).addCallbacks(return_value)
+        server.raspi.find_music_url(0,tags)#callRemote('raspi.find_music_url',0,url).addCallbacks(return_value)
         #reactor.run()
         # TODO : this is blocking, warning!!
     return json.dumps("haha")
@@ -155,12 +146,6 @@ def playMusic():
 def return_value(mess):
     print mess
     #reactor.stop()
-
-def search_music_generator(mixset):
-    for mix in mixset.mixes:
-        for song in mix:
-            yield song.data['url']# ou track_file_stream_url
-
 
 @app.route('/music')
 def music():
