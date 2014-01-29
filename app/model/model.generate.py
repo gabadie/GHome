afca64dbd7ea8f5d2994cbaee6135293a8af3317
Remi Domingues <remi.domingues@insa-lyon.fr>
2014-01-29 09:22:40 +0100

    Model generator minor changes

--- app/model/model.generate.py
+++ app/model/model.generate.py
@@ -22,16 +22,22 @@ class ModelGenerator:
         
             enocean.devices.Thermometer(device_id=1337, name='Living room thermometer', ignored=False).save()
             
-    def generate_sensors(self, device_class, device_number, readings_number, quantum):
+    def generate_devices(self, device_class, device_number):
         sensors = []
         for i in range(0, device_number):
             device = device_class(device_id=self.unique_id, ignored=False)
             device.save()
-            self.generate_readings(device, readings_number, quantum)
             sensors.append(device)
         return sensors
             
     def generate_readings(self, device, readings_number, quantum):
+        """
+        Generates random thermometer (temperature and humidity) readings
+        @param quantum The time (seconds) elapsed betweed two readings
+        """
+        time = datetime.datetime.now()
+        td = datetime.timedelta(seconds=quantum)
+        
         if isinstance(device, enocean.devices.Thermometer):
             call = self.generate_thermometer_readings
         elif isinstance(device, enocean.devices.Switch):
@@ -42,51 +48,29 @@ class ModelGenerator:
             call = self.generate_lms_readings
         else:
             raise NotImplementedError
+        
+        for i in range(0, readings_number):
+            call(device, time)
+            time += td
             
-        call(device, readings_number, quantum)
-            
-    def generate_thermometer_readings(self, device, readings_number, quantum):
-        """
-        Generates random thermometer (temperature and humidity) readings
-        @param quantum The time (seconds) elapsed betweed two readings
-        """
-        time = datetime.datetime.now()
-        td = datetime.timedelta(seconds=quantum)
-        
-        for i in range(0, readings_number):
-            model.devices.Temperature(device=device, value=round(random.random()*5+17, 2), date=time).save()
-            model.devices.Humidity(device=device, value=round(random.random()*50+25, 2), date=time).save()
-            time += td
+    def generate_thermometer_readings(self, device, time):
+        model.devices.Temperature(device=device, value=round(random.random()*5+17, 2), date=time).save()
+        model.devices.Humidity(device=device, value=round(random.random()*50+25, 2), date=time).save()
 
-    def generate_switch_readings(self, device, readings_number, quantum):
-        time = datetime.datetime.now()
-        td = datetime.timedelta(seconds=quantum)
-        
-        for i in range(0, readings_number):
-            pressed = random.choice([True, False])
-            if pressed:
-                model.devices.SwitchState(device=device, side=randrange(2)+1, direction=randrange(2)+3, pressed=pressed).save()
-            else:
-                model.devices.SwitchState(device=device, side=enocean.devices.Switch.UNKNOWN, direction=enocean.devices.Switch.UNKNOWN, pressed=pressed).save()
-            time += td
+    def generate_switch_readings(self, device, time):
+        pressed = random.choice([True, False])
+        if pressed:
+            model.devices.SwitchState(device=device, date=time, side=randrange(2)+1, direction=randrange(2)+3, pressed=pressed).save()
+        else:
+            model.devices.SwitchState(device=device, date=time, side=enocean.devices.Switch.UNKNOWN, direction=enocean.devices.Switch.UNKNOWN, pressed=pressed).save()
 
-    def generate_wc_readings(self, device, readings_number, quantum):
-        time = datetime.datetime.now()
-        td = datetime.timedelta(seconds=quantum)
-        
-        for i in range(0, readings_number):
-            model.devices.WindowState(device=device, value=random.choice([True, False])).save()
-            time += td
+    def generate_wc_readings(self, device, time):
+        model.devices.WindowState(device=device, date=time, value=random.choice([True, False])).save()
 
-    def generate_lms_readings(self, device, readings_number, quantum):
-        time = datetime.datetime.now()
-        td = datetime.timedelta(seconds=quantum)
-        
-        for i in range(0, readings_number):
-            model.devices.Voltage(device=device, value=round(random.random()*2+3, 2)).save()
-            model.devices.Brightness(device=device, value=round(random.random()*200+200)).save()
-            model.devices.Movement(device=device, value=random.choice([True, False])).save()
-            time += td
+    def generate_lms_readings(self, device, time):
+        model.devices.Voltage(device=device, date=time, value=round(random.random()*2+3, 2)).save()
+        model.devices.Brightness(device=device, date=time, value=round(random.random()*200+200)).save()
+        model.devices.Movement(device=device, date=time, value=random.choice([True, False])).save()
             
     def generate_reading_evolution(self, device, reading_class, readings_number, init_value, max_step_evolution, quantum):
         """
@@ -116,14 +100,22 @@ if __name__ == "__main__":
 
     generator = ModelGenerator(configuration)
     
-    sensors = generator.generate_sensors(enocean.devices.Thermometer, 2, 1, 60)
-    generator.generate_reading_evolution(sensors[0], model.devices.Temperature, 10, 15, 2, 60)
-    generator.generate_reading_evolution(sensors[1], model.devices.Humidity, 10, 62, 6, 60)
+    #Thermometer
+    devices = generator.generate_devices(enocean.devices.Thermometer, 3)
+    generator.generate_readings(devices[0], 5, 60)
+    generator.generate_reading_evolution(devices[1], model.devices.Temperature, 10, 15, 2, 60)
+    generator.generate_reading_evolution(devices[2], model.devices.Humidity, 10, 62, 6, 60)
     
-    generator.generate_sensors(enocean.devices.Switch, 2, 5, 120)
-    generator.generate_sensors(enocean.devices.WindowContact, 1, 10, 1800)
+    #Switch
+    devices = generator.generate_devices(enocean.devices.Switch, 2)
+    generator.generate_readings(devices[0], 5, 120)
+    devices = generator.generate_devices(enocean.devices.WindowContact, 1)
+    generator.generate_readings(devices[0], 10, 1800)
     
-    sensors = generator.generate_sensors(enocean.devices.LightMovementSensor, 1, 20, 300)
-    generator.generate_reading_evolution(sensors[0], model.devices.Brightness, 24, 50, 20, 900)
+    #LightMovementSensor
+    devices = generator.generate_devices(enocean.devices.LightMovementSensor, 1)
+    generator.generate_readings(devices[0], 20, 300)
+    generator.generate_reading_evolution(devices[0], model.devices.Brightness, 24, 50, 20, 900)
     
-    #idem capteur presence + 3 lampes
+    #Lamp
+    devices = generator.generate_devices(enocean.devices.Lamp, 3)
