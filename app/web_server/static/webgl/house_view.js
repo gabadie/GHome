6ba13d425f61ff72d88c39ad7af2439ed2e91dcc
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-02-02 16:32:59 +0100

    WebGL: Implements House and HouseRect classes

--- app/web_server/static/webgl/house_view.js
+++ app/web_server/static/webgl/house_view.js
@@ -1,6 +1,46 @@
 
+function HouseRect(x, y, w, h)
+{
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+}
+
 function House()
 {
+    this.rooms = new Array();
+
+    this.addRoom = function(x, y, w, h)
+    {
+        this.rooms.push(new HouseRect(x, y, w, h))
+    }
+
+    this.getBoundingBox = function()
+    {
+        var min_x = this.rooms[0].x;
+        var min_y = this.rooms[0].y;
+        var max_x = this.rooms[0].x + this.rooms[0].w;
+        var max_y = this.rooms[0].y + this.rooms[0].h;
+
+        for (var i = 1; i < this.rooms.length; i++)
+        {
+            min_x = Math.min(min_x, this.rooms[i].x);
+            min_y = Math.min(min_y, this.rooms[i].y);
+            max_x = Math.max(max_x, this.rooms[i].x + this.rooms[i].w);
+            max_y = Math.max(max_y, this.rooms[i].y + this.rooms[i].h);
+        }
+
+        return HouseRect(min_x, min_y, max_x - min_x, max_y - min_y);
+    }
+
+}
+
+function HouseWall(house)
+{
+    this.walls = new Array();
+
+
 
 }
 
@@ -8,6 +48,7 @@ function HouseView(output, canvas_id, house)
 {
     this.output = output;
     this.canvas = document.getElementById(canvas_id);
+    this.house = house;
 
     this.viewport = new Viewport();
     this.camera = new Camera();
@@ -33,15 +74,55 @@ function HouseView(output, canvas_id, house)
         this.draw()
     }
 
+    this.update_model = function()
+    {
+
+        this.update()
+    }
+
     this.draw = function()
     {
-        gl = this.gl;
+        var gl = this.gl;
 
         gl.clearColor(0.2, 0.5, 1.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
     }
 
-    this.update()
+    this.load = function()
+    {
+        var gl = this.gl;
 
+        var vertex_shader_code = [
+            "attribute vec3 vertex;",
+            "uniform mat4 modelScreenMatrix;",
+            "void main() {",
+                "gl_Position = modelScreenMatrix * vec4(vertex, 1.0);",
+            "}"
+        ].join("\n");
+
+        var fragment_shader_code = [
+            "void main() {",
+                "gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);",
+            "}"
+        ].join("\n");
+
+        var vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+        gl.shaderSource(vertex_shader, vertex_shader_code);
+        gl.compileShader(vertex_shader);
+
+        var fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(fragment_shader, fragment_shader_code);
+        gl.compileShader(fragment_shader);
+
+        var program = gl.createProgram();
+        gl.attachShader(program, vertex_shader);
+        gl.attachShader(program, fragment_shader);
+        gl.linkProgram(program);
+
+        this.program = program;
+    }
+
+    this.load();
+    this.update_model();
 }
 
