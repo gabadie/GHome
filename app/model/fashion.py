0e749f2c1f2d5d0408ad32631ee7d6f224f2b816
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-02-03 20:42:17 +0100

    Finished a first version of the Product class

--- app/model/fashion.py
+++ app/model/fashion.py
@@ -4,14 +4,44 @@ sys.path.append('../../libs')
 
 from shopsense.shopstyle import ShopStyle
 from config import GlobalConfig
+import mongoengine
 
 config = GlobalConfig()
+mongoengine.connect(config.mongo_db)
+shopstyle = ShopStyle(config.api_shopstyle)
 
-s = ShopStyle(config.api_shopstyle)
+
+class Product(mongoengine.Document):
+    id = mongoengine.fields.IntField(required=True, primary_key=True)
+    name = mongoengine.fields.StringField(required=True)
+    url = mongoengine.fields.StringField(required=True)
+
+    description = mongoengine.fields.StringField(required=True)
+    price = mongoengine.fields.IntField(required=True)
+    price_label = mongoengine.fields.StringField(required=True)
+
+
+    retailer = mongoengine.fields.DictField(required=True)
+    brand = mongoengine.fields.DictField(required=True)
+    categories = mongoengine.fields.ListField(mongoengine.fields.DictField(required=True))
+
+    images = mongoengine.fields.ListField(mongoengine.fields.DictField(required=True))
+
+    @staticmethod
+    def from_data(data):
+        data['url'] = data['pageUrl']
+        data['images'] = [c['image']['sizes'] for c in data['colors']]
+        data['price_label'] = data['priceLabel']
+        product = Product(**data)
+        return product
+
+    @staticmethod
+    def from_pid(pid):
+        data = shopstyle.product(pid)
+        data['id'] = pid
+        return Product.from_data(data)
 
 if __name__ == '__main__':
     # mens-clothes
     # 'women'
-    print [c['id'] for c in s.categories()['categories'] if 'pants' in c['id'].lower()]
-
-
+    print Product.from_pid(429844438).save().to_json()
